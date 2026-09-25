291892d6ad4f2c0d8df4b55bdfb148f8ff39acaf
chirz69 <chirasmitchoudhury05@gmail.com>
2026-09-26 01:24:26 +0530

    Delete start-dev.bat

--- start-dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0\backend"
-start "comment2dm-backend" cmd /k "python -m uvicorn main:app --host 127.0.0.1 --port 8000"
-cd /d "%~dp0\frontend\frontend"
-start "comment2dm-frontend" cmd /k "npm start"
